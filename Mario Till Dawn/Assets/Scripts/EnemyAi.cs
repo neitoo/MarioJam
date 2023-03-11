7666bd6caf18609574dfba1e0b43bf6b39c28270
neitoo <samuraa855@gmail.com>
2023-03-11 15:48:25 +0500

    add enemy

---
+++ Mario Till Dawn/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool shouldRotate;
+}
