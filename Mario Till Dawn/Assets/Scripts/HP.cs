a2668d37e7f195afed0d7d2bbe51aaba45f44c97
KrekIzTashkenta <90395810+KrekIzTashkenta@users.noreply.github.com>
2023-03-12 12:55:47 +0500

    hp

---
+++ Mario Till Dawn/Assets/Scripts/HP.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HP : MonoBehaviour
+{
+    public int hp;
+    public Text hpt;
+    public int damage;
+    public float timeRemaining = 20 * 60;
+
+    void Start()
+    {
+        hpt.text = "Hp " + hp.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            hp = hp - damage;
+            hpt.text = "Hp " + hp.ToString();
+        }
+    }
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            // Записать время в память устройства
+            PlayerPrefs.SetFloat("RemainingTime", timeRemaining);
+
+            // Загрузить сцену GameOver
+            SceneManager.LoadScene("GameOver");
+
+            // Остановить таймер
+            Timer timer = FindObjectOfType<Timer>();
+            timer.enabled = false;
+        }
+    }
+}
