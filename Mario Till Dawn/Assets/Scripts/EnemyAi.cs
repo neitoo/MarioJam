51159391db4def6ed9d7d52ac9732c00776e8e27
KrekIzTashkenta <90395810+KrekIzTashkenta@users.noreply.github.com>
2023-03-12 11:47:51 +0500

    g

--- Mario Till Dawn/Assets/Scripts/EnemyAi.cs
+++ Mario Till Dawn/Assets/Scripts/EnemyAi.cs
@@ -15,43 +15,49 @@ public class EnemyAi : MonoBehaviour
     private Transform target;
     private Rigidbody2D rb;
     private Animator anim;
-    private Vector2 movement;
     public Vector3 dir;
 
     private bool isInChaseRange;
     private bool isInAttackRange;
 
-    private void Start(){
+    private void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
-    private void Update(){
+    private void Update()
+    {
         anim.SetBool("isRunning", isInChaseRange);
 
-        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius,whatIsPlayer);
-        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius,whatIsPlayer);
-    
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+
         dir = target.position - transform.position;
-        float angle = Mathf.Atan2(dir.y,dir.x) *Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         dir.Normalize();
-        if(shouldRotate){
-            anim.SetFloat("X",dir.x);
-            anim.SetFloat("Y",dir.y);
+        if (shouldRotate)
+        {
+            anim.SetFloat("X", dir.x);
+            anim.SetFloat("Y", dir.y);
         }
     }
 
-    private void FixedUpdate() {
-        if(isInChaseRange && !isInAttackRange){
+    private void FixedUpdate()
+    {
+        if (isInChaseRange && !isInAttackRange)
+        {
             MoveCharacter(dir);
         }
-        if(isInAttackRange){
+        if (isInAttackRange)
+        {
             rb.velocity = Vector2.zero;
         }
     }
 
-    private void MoveCharacter(Vector2 dir){
-        rb.MovePosition((Vector2)transform.position + (dir * speed* Time.deltaTime));
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
     }
 
 }
