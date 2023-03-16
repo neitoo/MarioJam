935466c2d8d1767326ec7880da7f9d0ba460db32
neitoo <samuraa855@gmail.com>
2023-03-16 19:25:34 +0500

    add random generation heart

---
+++ Mario Till Dawn/Assets/Scripts/HeartSpawn.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSpawn : MonoBehaviour
+{
+    public GameObject prefab;
+    private float radius = 40f;
+    public float spawnInterval;
+    private int maxSpawnCount = 2;
+    private int spawnCount;
+    private float lastSpawnTime;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnPrefab());
+    }
+
+    private IEnumerator SpawnPrefab()
+    {
+        while (true)
+        {
+            if (spawnCount < maxSpawnCount && Time.time - lastSpawnTime > spawnInterval)
+            {
+                Vector2 spawnPosition = GetRandomSpawnPosition();
+                Instantiate(prefab, spawnPosition, Quaternion.identity);
+                spawnCount++;
+                lastSpawnTime = Time.time;
+            }
+            yield return null;
+        }
+    }
+
+    private Vector2 GetRandomSpawnPosition()
+    {
+        Vector3 randomPos = Random.insideUnitSphere * radius;
+        randomPos += transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(randomPos, 1f);
+        while (colliders.Length > 0)
+        {
+            randomPos = Random.insideUnitSphere * radius;
+            randomPos += transform.position;
+            colliders = Physics2D.OverlapCircleAll(randomPos, 1f);
+        }
+        return randomPos;
+    }
+}
